211f853f7bcb877f96628166a9592eb34db71e14
KaushikSR <kaushik.sadashiv.r@gmail.com>
2019-08-17 18:04:08 -0400

    modifications to SPI API

--- stm32f4xx_drivers/drivers/inc/stm32f407xx_spi.h
+++ stm32f4xx_drivers/drivers/inc/stm32f407xx_spi.h
@@ -8,18 +8,82 @@
 #ifndef INC_STM32F407XX_SPI_H_
 #define INC_STM32F407XX_SPI_H_
 
+#include "stm32f407xx.h"
+
+/***Configuration Enums***/
+
+typedef enum{
+	SLAVE = 0,
+	MASTER = 1
+}SPI_DevMode;
+
+typedef enum{
+	BIDI_TXRX = 0, // 2-line Unidirectional - Transmit and Receive
+	UNIDI_RX = 1, // 1-line Bidirectional - Receive only
+	UNIDI_TX = 2, // 1-line Bidirectional - Transmit only
+	BIDI_RX = 3 // 2-line Unidirectional - Receive only
+}SPI_BusMode;
+
+typedef enum{
+	DIV2 = 0,
+	DIV4 = 1,
+	DIV8 = 2,
+	DIV16 = 3,
+	DIV32 = 4,
+	DIV64 = 5,
+	DIV128 = 6,
+	DIV256 = 7
+}SPI_PreScalar;
+
+typedef enum{
+	BYTE = 0,
+	TWO_BYTE = 1
+}SPI_DataFrame;
+
+typedef enum{
+	IDLE0 = 0,
+	IDLE1 = 1
+}SPI_ClkPol;
+
+typedef enum{
+	LEADING_EDGE = 0,
+	TRAILING_EDGE = 1
+}SPI_ClkPhase;
+
+/***Register Enumerations ***/
+
+/*SPI_CR1 Register*/
+typedef enum{
+	CPHA = 0,
+	CPOL = 1,
+	MSTR = 2,
+	BR = 3,
+	SPE = 6,
+	LSBFIRST = 7,
+	SSI = 8,
+	SSM = 9,
+	RX_ONLY = 10,
+	DFF = 11,
+	CRC_NEXT = 12,
+	CRC_EN = 13,
+	BIDI_OE = 14,
+	BIDI_MODE = 15
+}SPI_CR1;
+
+
+
 /*
  *  Configuration structure for SPIx peripheral
  */
-
 typedef struct {
-	uint8_t SPI_DeviceMode;
-	uint8_t SPI_BusConfig;
-	uint8_t SPI_SclkSpeed;
-	uint8_t SPI_DFF;
-	uint8_t SPI_CPOL;
-	uint8_t SPI_CPHA;
-	uint8_t SPI_SSM;
+	SPI_DevMode SPI_DeviceMode;
+	SPI_BusMode SPI_BusConfig;
+	SPI_PreScalar SPI_SclkSpeed;
+	SPI_DataFrame SPI_DFF;
+	SPI_ClkPol SPI_CPOL;
+	SPI_ClkPhase SPI_CPHA;
+	status SPI_SSM;
+	status SPI_SSI;
 } SPI_Config_t;
 
 /*
@@ -28,12 +92,12 @@ typedef struct {
 typedef struct {
 	SPI_RegDef_t *pSPIx; /*!< This holds the base address of SPIx(x:1,2,3) peripheral >*/
 	SPI_Config_t SPIConfig;
-	uint8_t *pTxBuffer; /* !< To store the app. Tx buffer address > */
-	uint8_t *pRxBuffer; /* !< To store the app. Rx buffer address > */
-	uint32_t TxLen; /* !< To store Tx len > */
-	uint32_t RxLen; /* !< To store Tx len > */
-	uint8_t TxState; /* !< To store Tx state > */
-	uint8_t RxState; /* !< To store Rx state > */
+//	uint8_t *pTxBuffer; /* !< To store the app. Tx buffer address > */
+//	uint8_t *pRxBuffer; /* !< To store the app. Rx buffer address > */
+//	uint32_t TxLen; /* !< To store Tx len > */
+//	uint32_t RxLen; /* !< To store Tx len > */
+//	uint8_t TxState; /* !< To store Tx state > */
+//	uint8_t RxState; /* !< To store Rx state > */
 } SPI_Handle_t;
 
 #endif /* INC_STM32F407XX_SPI_H_ */
@@ -60,9 +124,9 @@ void SPI_Init(SPI_Handle_t* pSPIHandle);
  *@return : None
  * */
 
-void SPI_DeInit(SPI_Handle_t* pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
 
-/*@brief: These functions can be used to send/receive data (Polling method)
+/*@brief: These functions can be used to send/receive data (Polling/Blocking method)
  *@param[in] : A pointer to the SPI base address
  *@param[in] : A pointer to the Tx/Rx Buffer
  *@param[in] : Length of data/buffer
--- stm32f4xx_drivers/drivers/src/stm32f407xx_spi.c
+++ stm32f4xx_drivers/drivers/src/stm32f407xx_spi.c
@@ -5,3 +5,188 @@
  *      Author: kaushik
  */
 
+#include "stm32f407xx_spi.h"
+
+/*@brief: This function helps to configure the SPI peripheral Clock
+ *@param[in] : A pointer to the SPIx base address
+ *@param[in] : ENABLE or DISABLE
+ *@return : None
+ * */
+
+void SPI_PCLK_Config(SPI_RegDef_t *pSPIx, status ENorDI) {
+	if (ENorDI == ENABLE) {
+		pSPIx == SPI1 ? SPI1_PCLK_EN() :\
+ pSPIx == SPI2 ? SPI2_PCLK_EN() :\
+
+		pSPIx == SPI3 ? SPI2_PCLK_EN() : -1;
+	} else {
+		pSPIx == SPI1 ? SPI1_PCLK_DI() :\
+ pSPIx == SPI2 ? SPI2_PCLK_DI() :\
+
+		pSPIx == SPI3 ? SPI2_PCLK_DI() : -1;
+	}
+}
+
+/*@brief: This function helps to initialize the SPI peripheral
+ *@param[in] : A pointer to the SPI Handle data structure
+ *@return : None
+ * */
+
+void SPI_Init(SPI_Handle_t* pSPIHandle) {
+
+	SPI_PCLK_Config(pSPIHandle->pSPIx, ENABLE);
+
+	/*Mode*/
+	if (pSPIHandle->SPIConfig.SPI_DeviceMode) {
+		pSPIHandle->pSPIx->CR1 |= (1 << MSTR);
+	} else {
+		pSPIHandle->pSPIx->CR1 &= ~(1 << MSTR);
+	}
+	/*BusConfig*/
+	if (pSPIHandle->SPIConfig.SPI_BusConfig == BIDI_TXRX) {
+		pSPIHandle->pSPIx->CR1 &= ~(BIT15); // 2 line unidirectional
+	} else if (pSPIHandle->SPIConfig.SPI_BusConfig == UNIDI_RX) {
+		pSPIHandle->pSPIx->CR1 |= (BIT15); // 1 line Bidirectional
+		pSPIHandle->pSPIx->CR1 &= ~(BIT14); // Receive-Only mode
+	} else if (pSPIHandle->SPIConfig.SPI_BusConfig == UNIDI_TX) {
+		pSPIHandle->pSPIx->CR1 |= (BIT15); // 1 line Bidirectional
+		pSPIHandle->pSPIx->CR1 |= (BIT14); //Transmit-Only mode
+	} else if (pSPIHandle->SPIConfig.SPI_BusConfig == BIDI_RX) {
+		pSPIHandle->pSPIx->CR1 &= ~(BIT15); // 2 line unidirectional
+		pSPIHandle->pSPIx->CR1 |= (BIT10); // Receive-Only
+	}
+	/*ClockSpeed*/
+	pSPIHandle->pSPIx->CR1 &= ~(7U << BR); //clearing 3 bits [5:3]
+	pSPIHandle->pSPIx->CR1 |= (pSPIHandle->SPIConfig.SPI_SclkSpeed << BR);
+	/*Data Frame*/
+	if (pSPIHandle->SPIConfig.SPI_DFF) {
+		pSPIHandle->pSPIx->CR1 |= (1 << DFF);
+	} else {
+		pSPIHandle->pSPIx->CR1 &= ~(1 << DFF);
+	}
+	/*Clock Polarity*/
+	if (pSPIHandle->SPIConfig.SPI_CPOL) {
+		pSPIHandle->pSPIx->CR1 |= (1 << CPOL);
+	} else {
+		pSPIHandle->pSPIx->CR1 &= ~(1 << CPOL);
+	}
+	/*Clock Phase*/
+	if (pSPIHandle->SPIConfig.SPI_CPHA) {
+		pSPIHandle->pSPIx->CR1 |= (1 << CPHA);
+	} else {
+		pSPIHandle->pSPIx->CR1 &= ~(1 << CPHA);
+	}
+	/*Software slave management*/
+	if (pSPIHandle->SPIConfig.SPI_SSM) {
+		pSPIHandle->pSPIx->CR1 |= (1 << SSM);
+	} else {
+		pSPIHandle->pSPIx->CR1 &= ~(1 << SSM);
+	}
+	/*slave select information*/
+	if (pSPIHandle->SPIConfig.SPI_SSI) {
+		pSPIHandle->pSPIx->CR1 |= (1 << SSI);
+	} else {
+		pSPIHandle->pSPIx->CR1 &= ~(1 << SSI);
+	}
+}
+
+/*@brief: This function helps to deinitialize the SPI peripheral
+ *@param[in] : A pointer to the SPI Handle data structure
+ *@return : None
+ * */
+
+void SPI_DeInit(SPI_RegDef_t *pSPIx) {
+	if (pSPIx == SPI1) {
+		SPI1_DEINIT()
+		;
+	} else if (pSPIx == SPI2) {
+		SPI2_DEINIT()
+		;
+	} else if (pSPIx == SPI3) {
+		SPI3_DEINIT()
+		;
+	}
+}
+
+void SPI_Enable(SPI_RegDef_t *pSPIx) {
+	pSPIx->CR1 |= (BIT6);
+}
+
+void SPI_Disable(SPI_RegDef_t *pSPIx) {
+	pSPIx->CR1 &= ~(BIT6);
+}
+
+
+/*@brief: These functions can be used to send/receive data (Polling/Blocking method)
+ *@param[in] : A pointer to the SPI base address
+ *@param[in] : A pointer to the Tx/Rx Buffer
+ *@param[in] : Length of data/buffer
+ *@return : None
+ * */
+
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len) {
+	SPI_Enable(pSPIx);
+	while (Len > 0) {
+		while (!(pSPIx->SR & BIT1))
+			; // wait till TXE flag is set/Tx Buffer is empty
+		if (pSPIx->CR1 & BIT11) { // 16-BIT
+			pSPIx->DR = *((uint16_t*) pTxBuffer);
+			Len -= 2;
+			pTxBuffer += 2;
+		} else { // 8-BIT
+			pSPIx->DR = *pTxBuffer;
+			Len--;
+			pTxBuffer++;
+		}
+	}
+}
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len) {
+	SPI_Enable(pSPIx);
+	while (Len > 0) {
+		while (!(pSPIx->SR & BIT0))
+			; // wait till RXE flag is set/Rx Buffer is empty
+		if (pSPIx->CR1 & BIT11) { // 16-BIT
+			*((uint16_t*) pRxBuffer) = pSPIx->DR;
+			Len -= 2;
+			pRxBuffer += 2;
+		} else { // 8-BIT
+			*pRxBuffer = pSPIx->DR;
+			Len--;
+			pRxBuffer++;
+		}
+	}
+}
+
+/*
+ * @brief : This function ENABLES/DISABLES the given IRQ on the NVIC
+ *
+ * @param[in]: IRQ/Position number
+ * @param[in]: ENABLE/DISABLE
+ * @return : None
+ */
+void SPI_IRQITConfig(uint8_t IRQNumber, status EnOrDi) {
+
+}
+
+/*
+ * @brief : This function sets the priority of the given IRQ in the NVIC
+ *
+ * @param[in]: IRQ/Position number
+ * @param[in]: Priority
+ * @return : None
+ */
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority) {
+
+}
+
+/*
+ * @brief : This function clears the pending register of the EXTI controller for a particular EXTI line
+ *
+ * @param[in]: EXTI line number/Pin Number
+ * @return : None
+ *
+ * @Note : This must be called in all ISR functions for EXTI lines indicating the interrupt/event has been serviced
+ */
+void SPI_IRQHandle(uint8_t EXTI_line) {
+
+}
